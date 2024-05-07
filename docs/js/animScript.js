578468a1d84af40cdb675f6dccd65e24ee970176
shukla <destroyernv2004@gmail.com>
2024-05-07 17:58:08 -0400

    stuff

--- docs/js/animScript.js
+++ docs/js/animScript.js
@@ -7,3 +7,32 @@ window.addEventListener('scroll', () => {
     heading2.style.transform = 'translateX(0)'; // Slide the heading into view
   }
 });
+const heading3 = document.querySelectorAll('.sub-heading-2');
+
+window.addEventListener('scroll', () => {
+  heading3.forEach(subHeading => {
+    const subHeadingTop = subHeading.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (subHeadingTop < windowHeight) {
+      subHeading.style.transform = 'translateX(0)'; // Slide the heading into view
+    }
+  });
+});
+const logoContainers = document.querySelectorAll('.Logo-container');
+
+window.addEventListener('scroll', () => {
+  logoContainers.forEach(container => {
+    const containerTop = container.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (containerTop < windowHeight) {
+      container.style.transform = 'translateX(0)'; // Slide the container into view
+    }
+  });
+});
+
+function scrollToSection(className) {
+  var section = document.querySelector("." + className);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
